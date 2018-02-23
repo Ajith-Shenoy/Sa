7541b04113fd36a6d7fde03795bf77a600a2ddac
Ajith-Shenoy <Ajithshenoy19@yahoo.in>
2018-02-23 19:52:37 +0530

    Web View Fixed

--- app/src/main/java/in/edu/reva/myapplication/Web_Visit.java
+++ app/src/main/java/in/edu/reva/myapplication/Web_Visit.java
@@ -1,28 +1,46 @@
 package in.edu.reva.myapplication;
 
 import android.app.ActionBar;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-public class Web_Visit extends AppCompatActivity {
-
+public class Web_Visit extends AppCompatActivity
+{
+    WebView myWebView;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web__visit);
-        WebView myWebView = (WebView) findViewById(R.id.webview);
+
+        myWebView = (WebView) findViewById(R.id.webview);
         myWebView.loadUrl("https://saviskara.github.io/2018/");
         //Enable JS
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        //Inapp Navigation (One Way)
+        //Inapp Navigation
         myWebView.setWebViewClient(new WebViewClient());
 
     }
-
+    //back navigation Enabled
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
 
 }
+
