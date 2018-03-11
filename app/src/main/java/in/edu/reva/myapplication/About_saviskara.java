a5490585aa0922f9e6ddca54fca9a0ab8d49ae31
Ajith-Shenoy <Ajithshenoy19@yahoo.in>
2018-03-11 17:33:44 +0530

    Email bug in Contact Us section fixed.

--- app/src/main/java/in/edu/reva/myapplication/About_saviskara.java
+++ app/src/main/java/in/edu/reva/myapplication/About_saviskara.java
@@ -1,9 +1,10 @@
 package in.edu.reva.myapplication;
 
-import android.content.res.Configuration;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDelegate;
+import android.view.Gravity;
 import android.view.View;
 
 import mehdi.sakout.aboutpage.AboutPage;
@@ -11,48 +12,41 @@ import mehdi.sakout.aboutpage.Element;
 
 public class About_saviskara extends AppCompatActivity
 {
-
+    String Email ="saviskaracit@gmail.com";
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         // title bar name
         getSupportActionBar().setTitle("About Saviskara");
-        simulateDayNight(/* DAY */ 0);
-        Element adsElement = new Element();
         View aboutPage = new AboutPage(this)
                 .setDescription("Saviskara is a Techno-Cultural fest.\nOrganised by the school of Computing and Techmology.\nReva University Bangalore.\n" + "It's governed by RACIT ")
                 .isRTL(false)
                 .setImage(R.drawable.saviskaraboutus)
                 .addGroup("Connect with us")
-                .addEmail("saviskaracit@gmail.com")
+                //.addEmail("saviskaracit@gmail.com")
                 .addWebsite("https://saviskara.github.io/2018/")
                 .addFacebook("SCHOOL.C.IT/")
+                .addItem(email())
                 .create();
-
         setContentView(aboutPage);
     }
-    void simulateDayNight(int currentSetting)
-    {
-        final int DAY = 0;
-        final int NIGHT = 1;
-        final int FOLLOW_SYSTEM = 3;
+    // Custom Email element
+    Element email() {
+        Element copyRightsElement = new Element();
+        copyRightsElement.setIconDrawable(R.drawable.mail);
+        copyRightsElement.setGravity(Gravity.START);
+        copyRightsElement.setTitle(getString(R.string.emailUs));
+        copyRightsElement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:" + Email));
+                startActivity(intent);
+            }
+        });
+        return copyRightsElement;
 
-        int currentNightMode = getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK;
-        if (currentSetting == DAY && currentNightMode != Configuration.UI_MODE_NIGHT_NO)
-        {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_NO);
-        } else if (currentSetting == NIGHT && currentNightMode != Configuration.UI_MODE_NIGHT_YES)
-        {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_YES);
-        }
-        else if (currentSetting == FOLLOW_SYSTEM)
-        {
-            AppCompatDelegate.setDefaultNightMode(
-                    AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
-        }
     }
+
 }
--- app/src/main/java/in/edu/reva/myapplication/CulturalMain.java
+++ app/src/main/java/in/edu/reva/myapplication/CulturalMain.java
@@ -5,6 +5,7 @@ import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Window;
@@ -14,22 +15,28 @@ public class CulturalMain extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.LayoutManager layoutManager;
     RecyclerView.Adapter adapter;
-
+    CardView cardView;
+    //library
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cultural_main);
+        cardView =findViewById(R.id.cultCard);
+        //add status bar and action bar color
         Window window = this.getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.setStatusBarColor(ContextCompat.getColor(this, R.color.green));
+        //status bar set
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#009688")));
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        //color set
+        recyclerView = findViewById(R.id.recycler_view);
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
         //one line changes
         adapter = new CulturalRecyclerAdapter();
         recyclerView.setAdapter(adapter);
+
     }
 }
